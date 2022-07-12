eb6fa98e3ae0badc644b91fa0351434f912e4bff
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-07-12 23:51:11 +0300

    df

--- reactjs/my-app/components/Button.js
+++ reactjs/my-app/components/Button.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import Button from '@mui/material/Button';
+// import React from 'react';
+// import Button from '@mui/material/Button';
 
-export default function DisableElevation() {
-  return (
-    <Button variant="contained" disableElevation>
-      Disable elevation
-    </Button>
-  );
-}
+// export default function DisableElevation() {
+//   return (
+//     <Button variant="contained" disableElevation>
+//       Disable elevation
+//     </Button>
+//   );
+// }
+
+// import React from 'react';
+// import Button from '@mui/material/Button';
+
+// \
--- reactjs/my-app/src/App.js
+++ reactjs/my-app/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import Button from '../components/Button';
+// import Button from '../components/Button';
+import {Button, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   return (
     <>
-  <Button/>
+  {/* <Button/> */}
+  <div>
+  <Button variant="contained"  size="small" disableElevation>
+    Disable elevation
+  </Button>
+  <Button variant="outlined" startIcon={<DeleteIcon />} size="medium"> 
+        Delete
+      </Button>
+      <Typography variant="h1" component="h2">
+        h1 Heading but its h2 component
+      </Typography>
+      <button variant="contained" sx={{
+        backgroundColor:"skyblue",
+        color:"#888",
+        margin:5,
+      }}>My Uniqe Button</button>
+  </div>
     </>
   );
 }
