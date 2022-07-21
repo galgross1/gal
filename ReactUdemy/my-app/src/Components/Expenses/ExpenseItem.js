9bd288e5e6b35b41fa85e7398eb0d5dff3e88653
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-07-21 17:21:07 +0300

    fdg

--- ReactUdemy/my-app/src/Components/Expenses/ExpenseItem.js
+++ ReactUdemy/my-app/src/Components/Expenses/ExpenseItem.js
@@ -3,14 +3,22 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import React from 'react';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated!';
+    console.log(title);
+  };
+
   return (
     <Card className='expense-item'>
     <ExpenseDate date = {props.date}/>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
