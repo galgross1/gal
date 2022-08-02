2e488b8a1eca39ecad01b0095f89ad3d3c4fb485
Gal <galgross24@gmail.com>
2022-08-02 19:09:19 +0300

    כע

--- dockerDevOps/api/app.js
+++ dockerDevOps/api/app.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors"); //CORS is an HTTP-header based mechanism that allows a server to indicate any origins
+// (domain, scheme, or port) other than its own from which a browser should permit loading resources.
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json([
     {
-      "id":"1",
-      "title":"Book Review: The Bear & The Nightingale"
+      id: "1",
+      title: "Book Review: The Bear & The Nightingale",
     },
     {
-      "id":"2",
-      "title":"Game Review: Pokemon Brillian Diamond"
+      id: "2",
+      title: "Game Review: Pokemon Brillian Diamond",
     },
     {
-      "id":"3",
-      "title":"Show Review: Alice in Borderland"
-    }
-  ])
-})
+      id: "3",
+      title: "Show Review: Alice in Borderland",
+    },
+  ]);
+});
 
-app.listen(4000, () => {
-  console.log('listening for requests on port 4000')
-})
+app.listen(9090, () => {
+  console.log("listening for requests on port 9090");
+});
